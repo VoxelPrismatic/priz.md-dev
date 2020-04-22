2630bc742b605f359b4340442579826ffe6034f8
VoxelPrismatic <danielpetrov@outlook.com>
2020-04-21 19:10:54 -0500

    Fixed up some things 	modified:   iframe.js 	modified:   iframe.lite.js 	modified:   markup.js 	modified:   markup.lite.js 	modified:   md/elem/accent.js

--- iframe.js
+++ iframe.js
@@ -4,18 +4,16 @@ var script__ = document.createElement("iframe");
 script__.src = base__.slice(0, -4);
 script__.id = "prizmd";
 head__.after(script__);
-var script__ = document.createElement("script");
-script__.src = base__ + "md.min.js";
-head__.after(script__);
 var css__ = document.createElement("link");
 css__.rel = "stylesheet";
-css__.type = "text/stylesheet";
+css__.type = "text/css";
 css__.href = base__ + "style.min.css";
 head__.after(css__);
 delete css__;
 delete script__;
 delete base__;
 delete head__;
+
 
 markdown_content = "";
 function mark_page(st) {
