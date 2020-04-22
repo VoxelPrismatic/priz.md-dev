5f853481dca5d200e8cffcf0dcd67bacbff39414
VoxelPrismatic <danielpetrov@outlook.com>
2020-04-21 19:26:15 -0500

    More stuff 	modified:   markup.html 	new file:   md/extras/extras.js 	new file:   md/extras/find.js 	modified:   min.py 	modified:   out/md.min.js

---
+++ md/extras/extras.js
@@ -0,0 +1,11 @@
+function compSty(elem) {
+    try {
+        return window.getComputedStyle(elem);
+    } catch(err) {
+        try {
+            return window.getComputedStyle(find(elem));
+        } catch(err) {
+            return window.getComputedStyle(find(elem)[0]);
+        }
+    }
+}
